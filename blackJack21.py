ada7a3da4c2a275abb62458a70000f07f187533f
Ibrahem Elhorege <37155538+ielhoregr@users.noreply.github.com>
2024-03-07 18:11:30 +0200

    Initializing game rule & implementing The sart Function

---
+++ blackJack21.py
@@ -0,0 +1,21 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def start():
+    print("Do You Want to play 21 balckjack: y or n")
+    response = input().lower()
+    if response == 'y':
+        game()
+    else:
+        exit()
+
+
+def game():
+    player_cards = []
+    dealer_cards = []
+
+    print("game has started")
+    return
+
+
+start()
